3d784065a250cf02c62a799ff8b6d785fb057b95
egoroz <egorsemirenko04gmail.com>
2022-11-11 00:29:47 +0300

    last vertion

--- classwork09/Поймай шарик ver2.py
+++ classwork09/Поймай шарик ver2.py
@@ -14,12 +14,22 @@ text_surface = my_font.render('Ваш счёт: 0', False, (255, 255, 255))
 
 
 def new_ball():
-    """Рисует новый шарик  и дает ему начальную скорость"""
+    """Рисует новый шарик и дает ему начальную скорость"""
     global x, y, r, z
     x = randint(100, 800)
     y = randint(100, 600)
     r = randint(20, 60)
     z = randint(1, 16)
+    circle(screen, (50, 255, 0), (x, y), r)
+
+
+def death_ball():
+    """Рисует новый смертельный шарик  и дает ему начальную скорость"""
+    global x, y, r, z
+    x = randint(100, 800)
+    y = randint(100, 600)
+    r = randint(20, 60)
+    z = randint(1, 10)
     circle(screen, (50, 255, 0), (x, y), r)
 
 
@@ -29,6 +39,20 @@ def speed_balls(speeds, balls):
     for i in range(len(balls)):
         x1, y1, r1 = balls[i]
         vx, vy = speeds[i]
+        if r1 >= 900 - x1 or r1 >= x1:
+            vx = -vx
+        if r1 >= 700 - y1 or r1 >= y1:
+            vy = -vy
+        new_speeds.append((vx, vy))
+    return new_speeds
+
+
+def speed_death_balls(death_speeds, death_balls):
+    """Отражение смертельных шариков от краев """
+    new_speeds = []
+    for i in range(len(death_balls)):
+        x1, y1, r1 = death_balls[i]
+        vx, vy = death_speeds[i]
         if r1 >= 900 - x1 or r1 >= x1:
             vx = -vx
         if r1 >= 700 - y1 or r1 >= y1:
@@ -49,10 +73,25 @@ def move_balls(balls, speeds):
     return new_balls
 
 
-def show_balls(balls):
+def move_death_balls(death_balls, death_speeds):
+    """Движение шариков в одном направление """
+    new_balls = []
+    for i in range(len(death_balls)):
+        x1, y1, r1 = death_balls[i]
+        vx, vy = death_speeds[i]
+        x1 += vx
+        y1 += vy
+        new_balls.append((x1, y1, r1))
+    return new_balls
+
+
+def show_balls(balls, death_balls, color):
     for ball in balls:
         x1, y1, r1 = ball
         circle(screen, (50, 255, 0), (x1, y1), r1)
+    for ball in death_balls:
+        x1, y1, r1 = ball
+        circle(screen, (color), (x1, y1), r1)
 
 
 pygame.display.update()
@@ -60,8 +99,14 @@ clock = pygame.time.Clock()
 finished = False
 counter = 0
 balls = []
+death_balls = []
 speeds = []
-tick = 0
+death_speeds = []
+color = (50, 255, 0)
+tick1 = 0
+tick2 = 0
+tick3 = 0
+
 
 while not finished:
     clock.tick(FPS)
@@ -83,15 +128,47 @@ while not finished:
                     text_surface = my_font.render(f'Ваш счёт: {counter}', False, (255, 255, 255))
                     n -= 1
                 i += 1
+
+            i = 0
+            n = len(death_balls)
+            while i < n:
+                x, y, r = death_balls[i]
+                if (x - x1) ** 2 + (y - y1) ** 2 <= r ** 2:
+                    del death_balls[i]
+                    del death_speeds[i]
+                    counter -= 50
+                    text_surface = my_font.render(f'Ваш счёт: {counter}', False, (255, 255, 255))
+                    n -= 1
+                i += 1
+
     screen.fill((0, 0, 0))
     tick0 = pygame.time.get_ticks()
-    if tick0 - tick > 3000:
+
+    if tick0 - tick1 > 10000:
+        if len(death_balls) >= 2:
+            for i in range(len(death_balls)-1):
+                del death_balls[0]
+                del death_speeds[0]
+        tick1 = pygame.time.get_ticks()
+
+    if tick0 - tick3 > 3000:
         new_ball()
         speeds.append((7 - z, 9 - z))
         balls.append((x, y, r))
-        tick = pygame.time.get_ticks()
-    show_balls(balls)
+        death_ball()
+        death_balls.append((x, y, r))
+        death_speeds.append((2 - z, 3 - z))
+        color = (255, 10, 0)
+        tick3 = pygame.time.get_ticks()
+
+    if tick0 - tick3 > 500:
+        color = (50, 255, 0)
+
+    show_balls(balls, death_balls, color)
+    color = (255, 10, 0)
     balls = move_balls(balls, speeds)
+    death_balls = move_death_balls(death_balls, death_speeds)
+    death_speeds = speed_death_balls(death_speeds, death_balls)
     speeds = speed_balls(speeds, balls)
     screen.blit(text_surface, (0, 0))
     pygame.display.update()
