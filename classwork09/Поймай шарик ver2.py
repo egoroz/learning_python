993a90fa6873c047ef8e1d03ac6de6d774452863
egoroz <egorsemirenko04gmail.com>
2022-11-10 22:28:23 +0300

    add delete balls

--- classwork09/Поймай шарик ver2.py
+++ classwork09/Поймай шарик ver2.py
@@ -5,38 +5,22 @@ from random import randint
 pygame.init()
 pygame.font.init()
 
-FPS = 200
+FPS = 100
 x0 = 900
 y0 = 700
 screen = pygame.display.set_mode((x0, y0))
 my_font = pygame.font.SysFont('Comic Sans MS', 30)
-text_surface = my_font.render('Ваш счёт:', False, (255, 255, 255))
-
-RED = (255, 0, 0)
-BLUE = (0, 0, 255)
-YELLOW = (255, 255, 0)
-GREEN = (0, 255, 0)
-MAGENTA = (255, 0, 255)
-CYAN = (0, 255, 255)
-BLACK = (0, 0, 0)
-# временно
-COLORS = [YELLOW, YELLOW, YELLOW, YELLOW, YELLOW, YELLOW]
+text_surface = my_font.render('Ваш счёт: 0', False, (255, 255, 255))
 
 
 def new_ball():
-    """Рисует новый шарик """
-    global x, y, r
+    """Рисует новый шарик  и дает ему начальную скорость"""
+    global x, y, r, z
     x = randint(100, 800)
     y = randint(100, 600)
-    r = randint(10, 100)
-    color = COLORS[randint(0, 5)]
-    circle(screen, color, (x, y), r)
-
-
-def new_speed():
-    """Дает шарику начальную скорость """
-    global z
-    z = randint(1, 2)
+    r = randint(20, 60)
+    z = randint(1, 16)
+    circle(screen, (50, 255, 0), (x, y), r)
 
 
 def speed_balls(speeds, balls):
@@ -51,6 +35,7 @@ def speed_balls(speeds, balls):
             vy = -vy
         new_speeds.append((vx, vy))
     return new_speeds
+
 
 def move_balls(balls, speeds):
     """Движение шариков в одном направление """
@@ -67,8 +52,7 @@ def move_balls(balls, speeds):
 def show_balls(balls):
     for ball in balls:
         x1, y1, r1 = ball
-        color = COLORS[randint(0, 5)]
-        circle(screen, color, (x1, y1), r1)
+        circle(screen, (50, 255, 0), (x1, y1), r1)
 
 
 pygame.display.update()
@@ -80,7 +64,6 @@ speeds = []
 tick = 0
 
 while not finished:
-
     clock.tick(FPS)
 
     for event in pygame.event.get():
@@ -88,19 +71,23 @@ while not finished:
             finished = True
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x1, y1 = event.pos
-            for ball in balls:
-                x, y , r = ball
+            i = 0
+            n = len(balls)
+            while i < n:
+                x, y, r = balls[i]
+                vx, vy = speeds[i]
                 if (x - x1)**2 + (y - y1)**2 <= r**2:
-                    counter += 1
-                    text_surface = my_font.render('Some Text', False, (0, 0, 0))
-
-
-    screen.fill(BLACK)
+                    del balls[i]
+                    del speeds[i]
+                    counter += max(1, round(50*((vx**2 + vy**2)**0.5)//r))
+                    text_surface = my_font.render(f'Ваш счёт: {counter}', False, (255, 255, 255))
+                    n -= 1
+                i += 1
+    screen.fill((0, 0, 0))
     tick0 = pygame.time.get_ticks()
     if tick0 - tick > 3000:
         new_ball()
-        new_speed()
-        speeds.append((z, 4 - z))
+        speeds.append((7 - z, 9 - z))
         balls.append((x, y, r))
         tick = pygame.time.get_ticks()
     show_balls(balls)
